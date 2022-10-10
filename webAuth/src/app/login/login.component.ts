a975ab16f0dd1834439f18a564d3461a97e57405
Gazi Hataş <gazihatas71@gmail.com>
2022-10-10 17:23:21 +0300

    angular tarafından kullanıcı giriş yapıyot token geliyor.

--- webAuth/src/app/login/login.component.ts
+++ webAuth/src/app/login/login.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, Validators } from '@angular/forms';
+import { UserService } from '../services/user.service';
 
 @Component({
   selector: 'app-login',
@@ -13,14 +14,24 @@ export class LoginComponent implements OnInit {
     password:['',Validators.required]
   });
 
-  constructor(private formBuilder:FormBuilder) { }
+  constructor(private formBuilder:FormBuilder, private userService:UserService) { }
 
   ngOnInit(): void {
   }
 
   onSubmit()
   {
-    console.log("on submit");
+
+    console.log("Login | on submit");
+    //formdan gelen değişkenler
+    let email = this.loginForm.controls['email'].value;
+    let password = this.loginForm.controls['password'].value;
+    //userService mize değişkenlerimizi yolluyoruz.
+    this.userService.login(email, password).subscribe((data)=>{
+      console.log("response",data);
+    },error=>{
+      console.log("error",error);
+    })
   }
 
 }
