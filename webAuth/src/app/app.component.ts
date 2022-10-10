35f6a55fe703ae782472fd8a8be8c5ffb9373ff4
Gazi Hataş <gazihatas71@gmail.com>
2022-10-10 23:39:18 +0300

    Logout ve login ile giriş yappılmışsa logout ve user-management componentleri gelmiyor.

--- webAuth/src/app/app.component.ts
+++ webAuth/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,17 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'webAuth';
+
+  constructor(private router:Router) {}
+
+  onLogout()
+  {
+    localStorage.removeItem("userInfo");
+  }
+
+  get isUserlogin()
+  {
+    const user = localStorage.getItem("userInfo");
+    return user && user.length > 0;
+  }
 }
